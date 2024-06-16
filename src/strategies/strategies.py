d9b8ed12288e287473c0ec325107d355c01d788c
EMAlbright <ethanmacalbright@gmail.com>
2024-06-16 01:34:36 -0700

    backtest strats

--- src/strategies/strategies.py
+++ src/strategies/strategies.py
@@ -4,18 +4,33 @@ from backtesting.test import SMA, GOOG
 import pandas_datareader as web
 import datetime as dt
 import matplotlib.pyplot as plt
-from fastapi import FastAPI, HTTPException
 from typing import List
-from pydantic import BaseModel
+import flask
+import talib
 
-app = FastAPI()
+class RSI_Strategy(Strategy):
+    def init(self):
+        price = self.data.Close
+        # Calculate RSI
+        self.rsi = self.I(talib.RSI, price, timeperiod=14)
+    
+    def next(self):
+        if self.rsi[-1] < 30:  # RSI below 30 indicates oversold
+            self.buy()
+        elif self.rsi[-1] > 70:  # RSI above 70 indicates overbought
+            self.sell()
 
-class BacktestRequest(BaseModel):
-    symbol: str
-    startDate: str
-    endDate: str
-    strategy: str
-
+class BBStrategy(Strategy):
+    def init(self):
+        price = self.data.Close
+        self.upper, self.middle, self.lower = self.I(talib.BBANDS, price, timeperiod=20)
+    
+    def next(self):
+        if self.data.Close[-1] < self.lower[-1]:  # Price below lower band
+            self.buy()
+            # higher than sell
+        elif self.data.Close[-1] >= self.lower[-1]:
+            self.sell()
 class MovingAverageStrategy(Strategy):
     def init(self):
         price = self.data.Close
@@ -30,36 +45,70 @@ class MovingAverageStrategy(Strategy):
         elif crossover(self.ma2, self.ma1):
             self.sell()
 
+class MACDStrategy(Strategy):
+    def init(self):
+        price = self.data.Close
+        # Calculate MACD
+        macd, signal, _ = self.I(talib.MACD, price, fastperiod=12, slowperiod=26, signalperiod=9)
+        self.macd_line = macd - signal
+
+    def next(self):
+        if crossover(self.macd_line, 0):
+            self.buy()
+        elif crossover(0, self.macd_line):
+            self.sell()
+
+class CCIStrategy(Strategy):
+    def init(self):
+        self.cci = self.I(talib.CCI, self.data.High, self.data.Low, self.data.Close, timeperiod=20)
+
+    def next(self):
+        if self.cci[-1] > 100:
+            self.buy()
+        elif self.cci[-1] < -100:
+            self.sell()
+
+class sarStrategy(Strategy):
+    def init(self):
+        self.sar = self.I(talib.SAR, self.data.High, self.data.Low, acceleration=.02, maximum=.2)
+    
+    def next(self):
+        if crossover(self.data.Close, self.sar):
+            self.buy()
+        elif crossover(self.sar, self.data.Close):
+            self.sell()
+
 # start with 9 more strategies
 strategies = {
     "moving_sma": MovingAverageStrategy,
-    #RSI
-    #Bollinger Band
+    "rsi": RSI_Strategy,
+    "bollinger_band": BBStrategy,
+    "macd": MACDStrategy,
+    "cci": CCIStrategy,
+    "sar": sarStrategy
 }
 
-#start = dt.datetime(2024,1,1)
-#end = dt.datetime(2024,5,13)
-#data =web.DataReader("TSLA", "stooq", start, end)
-#backtest = Backtest(data, MovingAverageStrategy, commission=.002, exclusive_orders=True)
-#stats = backtest.run()
-#print(stats)
+start = dt.datetime(2020,1,1)
+end = dt.datetime(2024,5,13)
+data =web.DataReader("TSLA", "stooq", start, end)
+backtestSMA = Backtest(data, MovingAverageStrategy, commission=.002, exclusive_orders=True)
+backtestRSI = Backtest(data, RSI_Strategy, commission=.002, exclusive_orders=True)
+backtestBB = Backtest(data, BBStrategy, commission=.002, exclusive_orders=True)
+backtestMACD = Backtest(data, MACDStrategy, commission=.002, exclusive_orders=True)
+backtestCCI = Backtest(data, CCIStrategy, commission=.002, exclusive_orders=True)
+backtestSAR = Backtest(data, sarStrategy, commission=.002, exclusive_orders=True)
+statsSMA = backtestSMA.run()
+statsRSI = backtestRSI.run()
+statsBB = backtestBB.run()
+statsMACD = backtestMACD.run()
+statsCCI = backtestCCI.run()
+statsSAR = backtestSAR.run()
+print("SMA\n:", statsSMA)
+print("\nRSI:", statsRSI)
+print("\nBollingerBand:", statsBB)
+print("\nMACD:", statsMACD)
+print("\nCCI:", statsCCI)
+print("\nSAR:", statsSAR)
 #backtest.plot()
 
-@app.post("/strategies")
-async def run_backtest(request: BacktestRequest):
-    start = dt.datetime.strptime(request.startDate, '%Y-%m-%d')
-    end = dt.datetime.strptime(request.endDate, '%Y-%m-%d' )
-    data = web.DataReader(request.symbol, "stooq", start, end)
-
-    if request.strategy not in strategies:
-        raise HTTPException(status_code=400, detail="Not an available backtesting strategy")
-
-    strategy = strategies[request.strategy]
-    backtest = Backtest(data, strategy, commission=.002, exclusive_orders=True)
-    stats = backtest.run()
-    return stats._trades.to_dict()
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
 
