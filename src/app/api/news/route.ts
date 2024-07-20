ac6c939b72aa6cc7294c841e93d37fcf6f9af850
EMAlbright <ethanmacalbright@gmail.com>
2024-07-20 15:17:42 -0700

    update news api

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 export async function GET(){
     const key = process.env.NEWS_API;
-    const url = `https://newsapi.org/v2/top-headlines?category=business&language=en&country=us&apiKey=${key}`;
+    const url = `https://newsapi.org/v2/everything?q=stocks&language=en&sortBy=publishedAt&apiKey=${key}`;
 
     try {
         const res = await axios.get(url);
